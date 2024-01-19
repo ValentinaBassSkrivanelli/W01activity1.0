e9c7b11a300c03383b9d0a4fe1110a63b2830abf
ValentinaBassSkrivanelli <aldanavalentinabass@gmail.com>
2024-01-18 23:27:29 -0300

    new

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
+
+const port = process.env.PORT || 8080;
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
